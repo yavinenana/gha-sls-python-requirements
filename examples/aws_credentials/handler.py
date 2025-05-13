cbad7b2a67ebdae48d52dac927d7b19525c8212b
jpenab <jpenab@auna.pe>
2025-05-13 02:40:42 -0500

    deploy workdir aws_credentials and another  && add npm install ci

--- examples/aws_credentials/handler.py
+++ examples/aws_credentials/handler.py
@@ -4,7 +4,7 @@ import boto3
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event
     }
 
